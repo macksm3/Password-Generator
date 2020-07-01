07f9d5541e0ab4eb223d04cb07d77e0d50f34a18
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-07-01 15:01:42 -0400

    add select character set options

--- script.js
+++ script.js
@@ -3,15 +3,33 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   // declare the list of characters available to use in the password
-  const charOptions = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()";
+  const alphaLower ="abcdefghijklmnopqrstuvwxyz",
+        alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        numeric = "0123456789",
+        special = "!@#$%^&*()";
+  let charOptions = "";
+
+// charOptions = alphaLower + alphaUpper + numeric + special;
+
   // set the password length
   let pwLength = prompt("enter a number between 8 and 128");
-  let newPassword = "";
-  for (var i = 0; i < pwLength; i++) {
-  // build new password with random chatacters from the list
-  newPassword += charOptions.charAt(Math.floor(Math.random() * charOptions.length));
+  if (pwLength >= 8 && pwLength <= 128) {
+    // add character options
+    if (confirm("include lower case letters?")) charOptions = charOptions + alphaLower;
+    if (confirm("include upper case letters?")) charOptions = charOptions + alphaUpper;
+    if (confirm("include numbers?")) charOptions = charOptions + numeric;
+    if (confirm("include special characters?")) charOptions = charOptions + special;
+    // validate character selection
+    if (charOptions === "") return "please try again \n this time select characters sets to include";
+    // options complete
+    let newPassword = "";
+    for (var i = 0; i < pwLength; i++) {
+    // build new password with random chatacters from the list
+    newPassword += charOptions.charAt(Math.floor(Math.random() * charOptions.length));
+    }
+    return newPassword;
   }
-  return newPassword;
+  else return "invalid length, try again";
 }
 
 // Write password to the #password input
